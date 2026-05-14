0e50f41d7aeeb5b871327bbcff8b5cebac179264
sujithkumar49 <136475925+sujithkumar49@users.noreply.github.com>
2026-05-14 11:10:28 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Webhook Test Successful");
+        System.out.println("Webhook Test Successfull");
     }
 }
